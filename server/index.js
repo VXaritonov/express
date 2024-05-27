18bfd52e18612749617baf59bf39c3c8a011214c
VXaritonov <vitalik_xaritonov_99@mail.ru>
2024-05-28 00:02:05 +0300

    database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: '5959692',
+  database: 'test',
+});
 
 const app = express();
 const port = process.env.PORT || 5555;
@@ -8,6 +17,12 @@ const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 app.use(express.static(DIST_DIR));
+
+app.get('/api', (req, res) => {
+  pool.query("SELECT * FROM users", function(err, data) {
+    res.send(data);
+  });
+});
 
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 const Main = () => {
-    return <div>Main Page</div>
+    const [data, setData] = React.useState(null)
+
+    React.useEffect(() => {{
+        axios.get('/api').then((response) => {
+            setData(response.data)
+        });
+    }}, [])
+
+    return (
+        <div>
+            Main Page
+            {data?.map(({ id, age, name}) => {
+                return (
+                    <div key={id}>
+                        id - {id}, age - {age}, name - {name},
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Main
