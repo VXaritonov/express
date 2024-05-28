fd82998bc18a55ec22e635c5b11bfa5710949229
VXaritonov <vitalik_xaritonov_99@mail.ru>
2024-05-28 15:11:20 +0300

    docker compose

--- server/index.js
+++ server/index.js
@@ -3,20 +3,20 @@ const path = require('path');
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: '5959692',
-  database: 'test',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || "3306",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "5959692",
+  database: process.env.DB_NAME || "test",
 });
 
 const app = express();
-const port = process.env.PORT || 5555;
+const port = process.env.NODE_LOCAL_PORT || 5555;
 
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
-app.use(express.static(DIST_DIR));
+app.use(express.static(DIST_DIR)); 
 
 app.get('/api', (req, res) => {
   pool.query("SELECT * FROM users", function(err, data) {
